b13a74b1f11b570fa1d3e19c280eb12d810821dc
Galina A. Basargina <gala.basargina@yandex.ru>
2024-09-23 23:14:09 +0300

    Применила рекурсию в Postgresql

--- orders.sql
+++ orders.sql
@@ -3347,4 +3347,69 @@ insert into order_compositions ("order",product,price,amount,discount) values
 	 (11077,75,77.5,4,0.0),
 	 (11077,77,130.0,2,0.0);
 
-	
+------------------------------------
+
+select 
+ e.id,
+ e.chief,
+ e.last_name || ', ' || e.first_name as name,
+ e2.last_name || ', ' || e2.first_name as chiiiiief,
+ e3.last_name || ', ' || e3.first_name as CHIIIIEF
+from
+ employee e
+  left join employee e2 on (e2.id = e.chief)
+  left join employee e3 on (e3.id = e2.chief);
+  
+with recursive r(id) as (
+ select
+  *,
+  1 AS level
+ from employee
+ where chief is null
+   union
+ select
+  e.*,
+  r.level + 1 AS level
+ from employee e
+      join r on (e.chief = r.id)
+)
+select chief, id, level from r;
+
+------------------------------------------------
+--sort_str |chief|id |level|?column?           |
+-----------+-----+---+-----+-------------------+
+--2        |     |  2|    1| Новиков, Павел    |
+--2|1      |    2|  1|    2|  Белова, Мария    |
+--2|3      |    2|  3|    2|  Бабкина, Ольга   |
+--2|4      |    2|  4|    2|  Воронова, Дарья  |
+--2|5      |    2|  5|    2|  Кротов, Андрей   |
+--2|5|6    |    5|  6|    3|   Акбаев, Иван    |
+--2|5|7    |    5|  7|    3|   Кралев, Петр    |
+--2|5|8    |    5|  8|    3|   Крылова, Анна   |
+--2|5|8|100|    8|100|    4|    Пупкин, Василий|
+--2|9      |    2|  9|    2|  Ясенева, Инна    |
+with recursive r(id) as (
+ select
+  *,
+  1 AS level,
+  id::varchar(255) as sort_str
+ from employee
+ where chief is null
+   union
+ select
+  e.*,
+  r.level + 1 AS level,
+  (r.sort_str || '|' || e.id::varchar(255))::varchar(255)
+ from employee e
+      join r on (e.chief = r.id)
+)
+select
+ sort_str,
+ chief, 
+ id, 
+ level, 
+ substring('           ' from 1 for level) || last_name || ', ' || first_name
+from r
+order by sort_str;
+
+  
